cb9c22ad0230bbb3fda9ad14f4c117e98ce75384
satishkurelly <google123>
2016-08-02 20:38:16 -0400

    remove body padding

--- app/resetpassword/resetpassword.js
+++ app/resetpassword/resetpassword.js
@@ -1,17 +1,1 @@
-﻿(function () {
-    'use strict';
-
-    angular
-        .module('app')
-        .controller('ResetController', ResetController);
-
-    ResetController.$inject = ['UserService', '$location', '$rootScope'];
-    function ResetController(UserService, $location, $rootScope ) {
-
-
-
-
-
-    }
-
-})();
+﻿
