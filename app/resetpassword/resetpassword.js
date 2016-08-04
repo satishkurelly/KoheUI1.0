9a908db907ab76cc4200ba9c561adfa3d68fe5ad
satishkurelly <google123>
2016-08-03 22:05:37 -0400

    navigation to login on cancel

--- app/resetpassword/resetpassword.js
+++ app/resetpassword/resetpassword.js
@@ -1,1 +1,36 @@
-﻿
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('portal')
+        .controller('ResetController', ResetController);
+
+    ResetController.$inject = ['$location', '$scope', '$http'];
+    function ResetController($location, $scope, $http) {
+
+            $scope.rstSubmit = function() {
+
+                //var username = $scope.userN;
+
+
+
+
+            };
+
+        $scope.rstCancel = function() {
+
+            //var username = $scope.userN;
+
+            $location.url("/login");
+
+
+        };
+
+
+
+
+
+
+    }
+
+})();
